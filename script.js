636428a04869bcab41e2fe8b93c8da6dbd981a51
Cale111 <34945905+Cale111@users.noreply.github.com>
2021-06-24 19:27:24 -0700

    more animations

--- script.js
+++ script.js
@@ -1,3 +1,27 @@
 var background = document.getElementById('background');
+var search = document.getElementById('search');
+var input = document.getElementById('input');
+var timeout;
 
 setTimeout(function(){ background.classList.add('fadein'); }, 500)
+setTimeout(function(){
+    search.classList.add('fadein');
+    search.classList.add('fullwidth');
+}, 2000)
+
+document.onmousemove = function() {
+    search.classList.add('fadein');
+    search.classList.add('fullwidth');
+    document.body.style.cursor = 'auto';
+    clearTimeout(timeout);
+
+    timeout = setTimeout(function() {
+        if (input && input.value) {
+            search.classList.remove('fadein')
+        } else {
+            search.classList.remove('fadein')
+            search.classList.remove('fullwidth')
+        }
+        document.body.style.cursor = 'none';
+    }, 5000)
+}
