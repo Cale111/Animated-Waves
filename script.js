feaa03f1ca928f7a3ef9b51ccbdfc575c4895389
Cale111 <34945905+Cale111@users.noreply.github.com>
2021-06-24 19:29:16 -0700

    comments

--- script.js
+++ script.js
@@ -1,20 +1,25 @@
+// Element variables
 var background = document.getElementById('background');
 var search = document.getElementById('search');
 var input = document.getElementById('input');
+// Timeout for mouse movement
 var timeout;
 
+// Startup fade in animation
 setTimeout(function(){ background.classList.add('fadein'); }, 500)
 setTimeout(function(){
     search.classList.add('fadein');
     search.classList.add('fullwidth');
 }, 2000)
 
+// Show ui when mouse moved
 document.onmousemove = function() {
     search.classList.add('fadein');
     search.classList.add('fullwidth');
     document.body.style.cursor = 'auto';
     clearTimeout(timeout);
 
+    // Hide ui if mouse not moved for 5 seconds
     timeout = setTimeout(function() {
         if (input && input.value) {
             search.classList.remove('fadein')
